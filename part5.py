e5aa20120a1caae6b7e272bea5320d2fac542030
Bilal11123 <156517790+Bilal11123@users.noreply.github.com>
2024-01-29 12:09:46 +0500

    Create part5.py

---
+++ part5.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+# Create a 3x3x3 array with random values
+random_array = np.random.rand(3, 3, 3)
+
+# Print the resulting array
+print("3x3x3 Array with Random Values:")
+print(random_array)
