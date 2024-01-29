ce5c4cf61e9351422bfea7f7e6d4a41dbae73dbf
Bilal11123 <156517790+Bilal11123@users.noreply.github.com>
2024-01-29 12:01:45 +0500

    Create part2.py

---
+++ part2.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+# Generate an array of 15 random numbers from a standard normal distribution
+random_numbers = np.random.randn(15)
+
+# Print the resulting array
+print(random_numbers)
